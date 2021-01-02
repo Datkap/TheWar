a6b52238b670415f9bdf4f1821a252d213e25ce6
Datkap <k.dzida@datkap.com>
2021-01-02 19:06:21 +0100

    Updated gameplay and TheWar Mechanism. Needs complete rework.

--- thewar.py
+++ thewar.py
@@ -41,19 +41,6 @@ for suit in card_suits:
             if card_values[card] >= 9:
                 small_deck.append(PlayingCard(card, suit))
 
-# Ask user with which deck he/she would like to play
-"""
-### DISABLED FOR TESTING PURPOSES ###
-
-question = [inquirer.List('Size of deck', message='Which deck would you like to play?', choices=['Full deck', 'Small deck'])]
-
-answer = inquirer.prompt(question)
-
-if answer == 'Full deck':
-    game_deck = full_deck[:]
-else:
-    game_deck = small_deck[:]
-"""
 # Shuffle chosen deck and deal cards to the players
 game_deck = small_deck[:]
 
@@ -64,16 +51,33 @@ cut = int(len(game_deck) / 2)
 p1_deck = game_deck[:cut]
 p2_deck = game_deck[cut:]
 
-# Play a hand
 p1_collected_stack = []
 p2_collected_stack = []
 
 def play():
+    
+    global p1_deck
+    global p2_deck
+    global p1_collected_stack
+    global p2_collected_stack
 
-    p1_played_card = p1_deck.pop()
-    p2_played_card = p2_deck.pop()
+    if len(p1_deck) == 0:
+        p1_deck = p1_collected_stack
+        p1_collected_stack = []
+        p1_played_card = p1_deck.pop()
+    elif len(p1_deck) == 0 and len(p1_collected_stack) == 0:
+        print('Game over, player 2 won.')
+    else:
+        p1_played_card = p1_deck.pop()
+    if len(p2_deck) == 0:
+        p2_deck = p2_collected_stack
+        p2_collected_stack = []
+        p2_played_card = p2_deck.pop()
+    elif len(p2_deck) == 0 and len(p2_collected_stack) == 0:
+        print('Game over, player 1 won.')    
+    else:
+        p2_played_card = p2_deck.pop()
 
-# Decide the result of the hand
     if card_values[p1_played_card.value] > card_values[p2_played_card.value]:
         print(f"Player 1 wins the hand with {p1_played_card.value} of {p1_played_card.suit}.")
         print(f"Player 2 lost the hand with {p2_played_card.value} of {p2_played_card.suit}.")
@@ -92,15 +96,71 @@ def play():
         print(f"Player 1 collected stack: {len(p1_collected_stack)}.")
         print(f"Player 2 stack: {len(p2_deck)}.")
         print(f"Player 2 collected stack: {len(p2_collected_stack)}.")
-# The War mechanism
     else:
         print("The WAR!")
+        if len(p1_deck) == 0:
+            p1_deck = p1_collected_stack
+            p1_collected_stack = []
+            p1_flipped_card = p1_deck.pop()
+        elif len(p1_deck) == 0 and len(p1_collected_stack) == 0:
+            print('Game over, player 2 won.')
+        else:
+            p1_flipped_card = p1_deck.pop()
 
+        if len(p2_deck) == 0:
+            p2_deck = p2_collected_stack
+            p2_collected_stack = []
+            p2_flipped_card = p2_deck.pop()
+        elif len(p2_deck) == 0 and len(p2_collected_stack) == 0:
+            print('Game over, player 1 won.')    
+        else:
+            p2_flipped_card = p2_deck.pop()
 
-# Collect won cards to winners collected_stack
+        if len(p1_deck) == 0:
+            p1_deck = p1_collected_stack
+            p1_collected_stack = []
+            p1_played_card = p1_deck.pop()
+        elif len(p1_deck) == 0 and len(p1_collected_stack) == 0:
+            print('Game over, player 2 won.')
+        else:
+            p1_played_card = p1_deck.pop()
+        if len(p2_deck) == 0:
+            p2_deck = p2_collected_stack
+            p2_collected_stack = []
+            p2_played_card = p2_deck.pop()
+        elif len(p2_deck) == 0 and len(p2_collected_stack) == 0:
+            print('Game over, player 1 won.')    
+        else:
+            p2_played_card = p2_deck.pop()
 
-
-# When players stack runs empty take up the collected_stack as new player's stack
-
-
-# end the game when one of the players remains without cards
+        if card_values[p1_played_card.value] > card_values[p2_played_card.value]:
+            print(f"Player 1 wins the hand with {p1_played_card.value} of {p1_played_card.suit}.")
+            print(f"Player 2 lost the hand with {p2_played_card.value} of {p2_played_card.suit}.")
+            p1_collected_stack.append(p1_played_card)
+            p1_collected_stack.append(p2_played_card)
+            p1_collected_stack.append(p1_flipped_card)
+            p1_collected_stack.append(p2_flipped_card)
+            print(f"Player 1 stack: {len(p1_deck)}.")
+            print(f"Player 1 collected stack: {len(p1_collected_stack)}.")
+            print(f"Player 2 stack: {len(p2_deck)}.")
+            print(f"Player 2 collected stack: {len(p2_collected_stack)}.")
+        elif card_values[p1_played_card.value] < card_values[p2_played_card.value]:
+            print(f"Player 2 wins the hand with {p2_played_card.value} of {p2_played_card.suit}.")
+            print(f"Player 1 lost the hand with {p1_played_card.value} of {p1_played_card.suit}.")        
+            p2_collected_stack.append(p2_played_card)
+            p2_collected_stack.append(p1_played_card)
+            p2_collected_stack.append(p2_flipped_card)
+            p2_collected_stack.append(p1_flipped_card)
+            print(f"Player 1 stack: {len(p1_deck)}.")
+            print(f"Player 1 collected stack: {len(p1_collected_stack)}.")
+            print(f"Player 2 stack: {len(p2_deck)}.")
+            print(f"Player 2 collected stack: {len(p2_collected_stack)}.")
+        else:
+            print(f"Player 1 stack: {len(p1_deck)}.")
+            print(f"Player 1 collected stack: {len(p1_collected_stack)}.")
+            print(f"Player 2 stack: {len(p2_deck)}.")
+            print(f"Player 2 collected stack: {len(p2_collected_stack)}.") 
+        print(f"Player 1 stack: {len(p1_deck)}.")
+        print(f"Player 1 collected stack: {len(p1_collected_stack)}.")
+        print(f"Player 2 stack: {len(p2_deck)}.")
+        print(f"Player 2 collected stack: {len(p2_collected_stack)}.")
